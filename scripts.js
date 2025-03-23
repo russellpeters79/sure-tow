b6c57e270eebd41a916b4e83e00fe97bfe5c09d9
russellpeters79 <russellpeters79@googlemail.com>
2025-03-23 11:11:09 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('contactButton');
     button.addEventListener('click', () => {
-        alert('Thank you for reaching out!');
+        alert('call 07512 093921');
     });
 });
